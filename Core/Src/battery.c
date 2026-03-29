6e269d3087ed58f6a26ca0bbe86edf4c2d28cdeb
nekiyor2 <nikita.deledivka@gmail.com>
2026-03-29 17:12:27 +0300

    Add error handling for ADC conversion
    Check for ADC conversion success before reading value.

--- Core/Src/battery.c
+++ Core/Src/battery.c
@@ -18,8 +18,9 @@ void Battery_Init(void)
 float Battery_ReadVoltage(void)
 {
     HAL_ADC_Start(&hadc1);
-    HAL_ADC_PollForConversion(&hadc1, 100);
-
+    if (HAL_ADC_PollForConversion(&hadc1, 100)!= HAL_OK)
+    return -1.0f; //сигнал помилки
+    
     uint16_t adc_value = HAL_ADC_GetValue(&hadc1);
 
     float v_adc     = (adc_value / 4095.0f) * 3.3f; // Перевід у напругу (0–3.3V)
