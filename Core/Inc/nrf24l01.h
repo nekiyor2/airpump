697a0d11cd7c462eab6e1bf05923c7b92d0aff3a
nekiyor2 <nikita.deledivka@gmail.com>
2026-03-29 17:16:24 +0300

    Refactor nrf24l01.h header guard and add commands

--- Core/Inc/nrf24l01.h
+++ Core/Inc/nrf24l01.h
@@ -5,9 +5,54 @@
  *      Author: nikit
  */
 
-#ifndef INC_NRF24L01_H_
-#define INC_NRF24L01_H_
+#ifndef INC_NRF24L01_H
+#define INC_NRF24L01_H
 
+#include "stm32c0xx_hal.h"
 
+// Піни
+#define NRF_CE_PORT     GPIOA
+#define NRF_CE_PIN      GPIO_PIN_3
+#define NRF_CSN_PORT    GPIOA
+#define NRF_CSN_PIN     GPIO_PIN_4
 
-#endif /* INC_NRF24L01_H_ */
+// Команди nRF24L01
+#define NRF_CMD_R_REG       0x00
+#define NRF_CMD_W_REG       0x20
+#define NRF_CMD_R_RX_PAYLOAD 0x61
+#define NRF_CMD_W_TX_PAYLOAD 0xA0
+#define NRF_CMD_FLUSH_TX    0xE1
+#define NRF_CMD_FLUSH_RX    0xE2
+#define NRF_CMD_NOP         0xFF
+
+// Регістри
+#define NRF_REG_CONFIG      0x00
+#define NRF_REG_EN_AA       0x01
+#define NRF_REG_EN_RXADDR   0x02
+#define NRF_REG_SETUP_AW    0x03
+#define NRF_REG_SETUP_RETR  0x04
+#define NRF_REG_RF_CH       0x05
+#define NRF_REG_RF_SETUP    0x06
+#define NRF_REG_STATUS      0x07
+#define NRF_REG_RX_ADDR_P0  0x0A
+#define NRF_REG_TX_ADDR     0x10
+#define NRF_REG_RX_PW_P0   0x11
+#define NRF_REG_FIFO_STATUS 0x17
+
+// Біти STATUS
+#define NRF_STATUS_RX_DR    (1 << 6)
+#define NRF_STATUS_TX_DS    (1 << 5)
+#define NRF_STATUS_MAX_RT   (1 << 4)
+
+// Розміри пакетів
+#define NRF_PAYLOAD_SIZE    12  // байт (підходить для обох структур)
+
+// Прототипи 
+void NRF24_Init(SPI_HandleTypeDef *hspi);
+void NRF24_SetRX(void);
+void NRF24_SetTX(void);
+uint8_t NRF24_DataReady(void);
+void NRF24_Receive(uint8_t *data);
+uint8_t NRF24_Transmit(uint8_t *data, uint8_t len);
+
+#endif
