0eaa8cf86b6e88f972198d51672418040fba3a97
nekiyor2 <nikita.deledivka@gmail.com>
2026-04-04 22:43:18 +0300

    Stop ADC and calculate battery voltage
    Stop ADC after reading value and calculate battery voltage.

--- Core/Src/battery.c
+++ Core/Src/battery.c
@@ -22,6 +22,7 @@ float Battery_ReadVoltage(void)
     return -1.0f; //сигнал помилки
     
     uint16_t adc_value = HAL_ADC_GetValue(&hadc1);
+     HAL_ADC_Stop(&hadc1);
 
     //ADC  12bit - 0...4095 = 0... 3.3v
     float v_adc     = (adc_value / 4095.0f) * 3.3f; // Перевід у напругу (0–3.3V)
