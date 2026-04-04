7e1261c2fa9f886f88d23872df652acb96cc6c29
nekiyor2 <nikita.deledivka@gmail.com>
2026-04-04 23:09:57 +0300

    Refactor initialization and remove _write function
    Removed unnecessary initialization calls and the custom _write function.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -105,15 +105,6 @@ int main(void)
 
   /* USER CODE BEGIN Init */
 	
-  SystemClock_Config();
-
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_SPI1_Init();
-  MX_ADC1_Init();
-  MX_USART1_UART_Init();
-  MX_TIM1_Init();
-	
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -483,11 +474,7 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-int _write(int file, char *ptr, int len)
-{
-    HAL_UART_Transmit(&huart1, (uint8_t*)ptr, len, HAL_MAX_DELAY);
-    return len;
-}
+
 /* USER CODE END 4 */
 
 /**
